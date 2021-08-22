469e515e1622ff0729d78294bee95e5ec19e9eb7
reykim7124 <mrayhanhakim2@gmail.com>
2021-08-22 17:10:03 +0700

    add weather card placeholder

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card height="205" max-width="324" min-width="324" flat> </v-card>
+</template>
+
+<style lang="sass" scoped>
+.v-card
+  background: $bLUEFlipped
+</style>
+
+<script>
+export default {
+  name: "WeatherCard",
+};
+</script>
