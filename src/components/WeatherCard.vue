32b76172c376e3be3624c91ca0ea27585319ffb6
reykim7124 <mrayhanhakim2@gmail.com>
2021-08-23 00:09:33 +0700

    implement openweathermap api

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,14 +1,109 @@
 <template>
-  <v-card height="205" max-width="324" min-width="324" flat> </v-card>
+  <v-card height="205" max-width="324" min-width="324" flat class="pa-2">
+    <div class="d-flex align-center pt-3">
+      <div>
+        <div class="v-card__subheader white--text px-4">
+          Chance of rain {{ precipitation }}%
+        </div>
+        <v-card-title class="text-capitalize white--text pt-0 v-card--big">{{
+          weather.weather[0].description
+        }}</v-card-title>
+      </div>
+      <v-img
+        :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"
+        contain
+        width="108"
+        height="103"
+        class="pa-3"
+      ></v-img>
+    </div>
+    <v-card-text class="pt-0 px-3 white--text">
+      <div class="d-flex align-center">
+        <v-icon dark left size="16">mdi-map-marker</v-icon>
+        {{ weather.name }}
+      </div>
+      <div class="py-3 d-flex justify-space-between align-end">
+        <v-btn
+          depressed
+          dark
+          class="ml-n1 pa-0 align-self-start"
+          color="transparent"
+        >
+          <div class="v-card__temp v-card--big mr-1">{{ calcTemp }}</div>
+          <div class="v-card__temp-symbol mt-n3 mr-1"></div>
+          <div class="d-flex v-card__temp-degree v-card--small">C</div>
+        </v-btn>
+        <div class="pa-1">
+          <v-icon size="16" dark>mdi-weather-hail</v-icon>
+          <span class="ml-1 v-card--small v-card__stat"
+            >{{ weather.main.humidity }}%</span
+          >
+        </div>
+        <div class="pa-1">
+          <v-icon size="16" dark>mdi-weather-sunny</v-icon>
+          <span class="ml-1 v-card--small v-card__stat">{{ weather.uvi }}</span>
+        </div>
+        <div class="pa-1">
+          <v-icon size="16" dark>mdi-weather-windy</v-icon>
+          <span class="ml-1 v-card--small v-card__stat"
+            >{{ calcWind }} mp/h</span
+          >
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <style lang="sass" scoped>
 .v-card
   background: $bLUEFlipped
+  .v-card__subheader
+    font-size: 0.875rem
+    font-weight: 500
+  .v-card--big
+    font-size: 1.5rem
+    font-weight: 700
+  .v-card__title
+    word-break: break-word
+  .v-card__temp-symbol
+    border: 1px solid white
+    width: 7px
+    height: 7px
+    border-radius: 7px
+  .v-card__temp-degree
+    margin-top: -11px
+    font-weight: 400
+  .v-card__stat
+    font-weight: 600
+  .v-card--small
+    font-size: 0.75rem
 </style>
 
 <script>
 export default {
   name: "WeatherCard",
+  data: () => ({
+    temp: "c",
+  }),
+
+  props: {
+    weather: Object,
+  },
+
+  computed: {
+    precipitation() {
+      return this.weather.pop * 100;
+    },
+
+    calcTemp() {
+      return this.temp
+        ? (this.weather.main.temp - 273.15).toFixed(0)
+        : (((this.weather.main.temp - 273.15) * 9) / 5 + 32).toFixed(0);
+    },
+
+    calcWind() {
+      return (this.weather.wind.speed * 2.237).toFixed(0);
+    },
+  },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,52 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+
+const appid = "fdf871cedaf3413c6a23230372c30a02";
+const weather = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&`;
+const forecast = `https://api.openweathermap.org/data/2.5/onecall?appid=${appid}&exclude=minutely,hourly,alerts,current&`;
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    weathers: [],
+  },
+  mutations: {
+    setWeathers: (state, payload) => {
+      state.weathers.push(payload);
+    },
+  },
+  actions: {
+    async getWeather({ commit, dispatch }, payload) {
+      try {
+        if (payload != null) {
+          const { data } = await axios.get(
+            `${weather}lat=${payload.lat}&lon=${payload.lon}`
+          );
+          const data2 = await dispatch("getForecast", data);
+          commit("setWeathers", data2);
+        }
+      } catch (error) {
+        //
+      }
+    },
+
+    async getForecast(context, payload) {
+      try {
+        const { data } = await axios.get(
+          `${forecast}lat=${payload.coord.lat}&lon=${payload.coord.lon}`
+        );
+        payload.uvi = data.daily[0].uvi;
+        payload.pop = data.daily[0].pop;
+        return payload;
+      } catch (error) {
+        //
+      }
+    },
+  },
+  getters: {
+    getWeathers: (state) => state.weathers,
+  },
   modules: {},
 });
