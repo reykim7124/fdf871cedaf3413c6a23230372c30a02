a8469557823e4e121d357cb9cbcd72b40a80f966
Reykim <mrayhanhakim2@gmail.com>
2024-11-03 05:34:48 +0700

    Update location.js

--- src/scripts/location.js
+++ src/scripts/location.js
@@ -1,7 +1,9 @@
+import store from "../store"
+
 function showPosition(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  this.$store.dispatch("getWeather", { lat: lat, lon: lon, type: "current" });
+  store.dispatch("getWeather", { lat: lat, lon: lon, type: "current" });
 }
 
 function getLocation() {
